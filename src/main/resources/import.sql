10536ff64775837e7d633e424c1a86ca75b28938
Wendel Julião <wendel.alves@sefaz.ce.gov.br>
2024-01-10 15:18:25 -0300

    first commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,12 @@
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$hu/8FCjU1PBsdUQD5D3NvusERBoFTrSanrDruC6VLqOcoufiRoAkC');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$hu/8FCjU1PBsdUQD5D3NvusERBoFTrSanrDruC6VLqOcoufiRoAkC');
+
+INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+
+INSERT INTO tb_product (name) VALUES ('TV');
+INSERT INTO tb_product (name) VALUES ('Computer');
